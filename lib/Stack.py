68986265489ca854b394f3afd525dc40068f3aea
Nili Scharf <nilischarf@gmail.com>
2025-03-04 11:28:08 +0200

    complete dsa stack

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,33 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items if items is not None else []
+        self.limit = limit 
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit 
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return self.items[::-1].index(target) 
+        return -1
